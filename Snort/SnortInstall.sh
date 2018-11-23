2d9f39582b9946f0c7fb2cfb4e7475e2a72e7c38
PHuhn <PhilHuhn@yahoo.com>
2018-11-23 12:12:00 -0500

    Testing install version 1

---
+++ Snort/SnortInstall.sh
@@ -0,0 +1,41 @@
+#
+# Installing Snort
+#  Written by: Phil Huhn
+#  Version 1
+#
+echo "=- Snort installation -="
+# Varialbes:
+snort_file=snort-2.9.12
+arm_pkg='-1_armhf'
+#
+# Let�s move back to the directory where we have been downloading our source code
+
+cd ~/sourcecode/snort_src/
+#
+# We now need to obtain the tar file of source code for Snort and decompress the file.  If you recall from earlier, go to snort.org to find this download.  Run the following commands to download and extract the tar file
+if [ -f ${snort_file}.tar.gz ]; then
+  rm ${snort_file}.tar.gz
+  rm -rf ${snort_file}
+fi
+echo "get ${snort_file}"
+wget https://snort.org/downloads/snort/${snort_file}.tar.gz
+tar xvfz ${snort_file}.tar.gz
+#
+# Installing Snort follows a similar process to compiling and installing the DAQ.  Lets move into the extracted directory. 
+
+cd ~/sourcecode/snort_src/${snort_file}
+#
+# Once inside the source directory, we can now begin the compile and install process.  Run the following commands
+
+./configure --enable-sourcefire
+make
+sudo checkinstall -D --install=no --fstrans=no
+#
+# Checkinstall will ask you some questions about the package to create.  Make the description something like �snort� and hit enter again.  Follow the prompts and use the defaults.  As we are not distributing the package, you will not have to worry about dependency identification.  Checkinstall will be useful should you choose to remove Snort and the DAQ at a later time
+# You will notice that if you list the directory contents, there will be a a file with .deb extension.  This is the package that checkinstall generated.  The name of this file can vary, so don�t follow the previous command verbatim.  Install it using the following command
+
+sudo dpkg -i ${snort_file}${arm_pkg}.deb
+#
+# It is also necessary to update the shared library cache.  Run the following command:
+echo "=- End of Snort installation -="
+#
