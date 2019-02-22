507b120f550c8abeeda92e36a369422c1cdc226c
PHuhn <PhilHuhn@yahoo.com>
2019-02-22 15:08:39 -0500

    raspberry-pi configuration

--- Python3_7.sh
+++ Python3_7.sh
@@ -7,6 +7,9 @@
 #
 PY_VER=3.7.2
 cd /tmp
+sudo apt update
+sudo apt upgrade
+sudo apt install gcc python3-dev python3-pip libxml2-dev libxslt1-dev zlib1g-dev g++
 sudo apt-get update -y
 sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
 wget https://www.python.org/ftp/python/${PY_VER}/Python-${PY_VER}.tar.xz
@@ -21,6 +24,20 @@ rm Python-${PY_VER}.tar.xz
 sudo apt-get --purge remove build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
 sudo apt-get autoremove -y
 sudo apt-get clean
-echo "now use python3.7 command to use this version."
+#
+sudo python3.7 -m pip install --upgrade pip
+#
+sudo apt update
+sudo apt upgrade
+sudo apt install gcc python3-dev python3-pip libxml2-dev libxslt1-dev zlib1g-dev g++
+#
+sudo python3.7 -m pip install gpiozero
+sudo python3.7 -m pip install rpi.gpio
+sudo python3.7 -m pip install pigpio
+sudo python3.7 -m pip install RPIO
+#
+sudo python3.7 -m pip install pyodbc
+#
 cd ~
+echo "now use python3.7 command to use this version."
 #
---
+++ btserial.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+# Taken from the following:
+# Headless Raspberry Pi configuration over Bluetooth by Patrick Hundal
+# https://hacks.mozilla.org/2017/02/headless-raspberry-pi-configuration-over-bluetooth/
+#
+# Edit the display name of the RaspberryPi so you can distinguish
+# your unit from others in the Bluetooth console
+# (very useful in a class setting)
+
+echo PRETTY_HOSTNAME=raspberrypi-01 > /etc/machine-info
+
+# Edit /lib/systemd/system/bluetooth.service to enable BT services
+sudo sed -i: 's|^Exec.*toothd$| \
+ExecStart=/usr/lib/bluetooth/bluetoothd -C \
+ExecStartPost=/usr/bin/sdptool add SP \
+ExecStartPost=/bin/hciconfig hci0 piscan \
+|g' /lib/systemd/system/bluetooth.service
+
+# create /etc/systemd/system/rfcomm.service to enable
+# the Bluetooth serial port from systemctl
+sudo cat <<EOF | sudo tee /etc/systemd/system/rfcomm.service > /dev/null
+[Unit]
+Description=RFCOMM service
+After=bluetooth.service
+Requires=bluetooth.service
+
+[Service]
+ExecStart=/usr/bin/rfcomm watch hci0 1 getty rfcomm0 115200 vt220
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# enable the new rfcomm service
+sudo systemctl enable rfcomm
+
+# start the rfcomm service
+sudo systemctl restart rfcomm
+
+# end of script
