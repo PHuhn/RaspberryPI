cf516805fc07e6684733908fbb90c443de395d02
PHuhn <PhilHuhn@yahoo.com>
2019-03-16 21:52:04 -0400

    Lint with www.shellcheck.net

--- Nagios/libexec/check_state_statusjson.sh
+++ Nagios/libexec/check_state_statusjson.sh
@@ -8,10 +8,11 @@
 # ============================================================================
 # 2019-02-05 V1.0.8 Phil
 # 2019-03-12 V1.0.9 Phil empty PASSWD forces only awk script
+# 2019-03-16 V1.0.10 Phil Lint with www.shellcheck.net
 #
 # program values
 PROGNAME=$(basename "$0")
-REVISION="1.0.9"
+REVISION="1.0.10"
 # exit codes
 STATE_OK=0
 STATE_WARNING=1
@@ -75,17 +76,17 @@ do
         S) SERVICE=${OPTARG};;
         U) USERID=${OPTARG};;
         P) PASSWD=${OPTARG};;
-        e) ESCALATE=`echo ${OPTARG} | tr '[:upper:]' '[:lower:]'`;;
-        l) LOGGING=`echo ${OPTARG} | tr '[:upper:]' '[:lower:]'`;;
-        v) VERBOSE=`echo ${OPTARG} | tr '[:upper:]' '[:lower:]'`;;
+        e) ESCALATE=$(echo "${OPTARG}" | tr '[:upper:]' '[:lower:]');;
+        l) LOGGING=$(echo "${OPTARG}" | tr '[:upper:]' '[:lower:]');;
+        v) VERBOSE=$(echo "${OPTARG}" | tr '[:upper:]' '[:lower:]');;
     esac
 done
 #
 if [ "${LOGGING}" == "true" ]; then
     LOG_FILE=/tmp/${PROGNAME}.log
-    if [ ! -f ${LOG_FILE} ]; then
-        echo "$$ ${PROGNAME} initializing ..." >> ${LOG_FILE}
-        chmod 666 ${LOG_FILE}
+    if [ ! -f "${LOG_FILE}" ]; then
+        echo "$$ ${PROGNAME} initializing ..." >> "${LOG_FILE}"
+        chmod 666 "${LOG_FILE}"
     fi
 else
     # if don't want LOG_FILE then change to /dev/null
@@ -95,39 +96,39 @@ fi
 if [ "${VERBOSE}" == "true" ]; then
     LOG_FILE=/dev/stdout
 fi
-echo "$$ ${PROGNAME} starting at $(date '+%Y-%m-%d %H:%M:%S') ..." >> ${LOG_FILE}
+echo "$$ ${PROGNAME} starting at $(date '+%Y-%m-%d %H:%M:%S') ..." >> "${LOG_FILE}"
 #
 QUERY="query=service&hostname=${HOST}&servicedescription=${SERVICE}"
-echo "$$ ${PROGNAME} ${QUERY}" >> ${LOG_FILE}
+echo "$$ ${PROGNAME} ${QUERY}" >> "${LOG_FILE}"
 FILE=/tmp/check_${SERVICE}_$$
 STATUS_FILE=/usr/local/nagios/var/status.dat
 STATE=
 OUTPUT=
 # can test with wrong password
 if [ "X${PASSWD}" != "X" ]; then
-    curl -v "http://${USERID}:${PASSWD}@localhost/nagios/cgi-bin/statusjson.cgi?${QUERY}" 1> ${FILE} 2> /dev/null
-    if [ -s ${FILE} ]; then
-        grep "last_hard_state.:" ${FILE} >/dev/null 2>&1
+    curl -v "http://${USERID}:${PASSWD}@localhost/nagios/cgi-bin/statusjson.cgi?${QUERY}" 1> "${FILE}" 2> /dev/null
+    if [ -s "${FILE}" ]; then
+        grep "last_hard_state.:" "${FILE}" >/dev/null 2>&1
         if [ $? == "0" ]; then
-            echo "$$ ${PROGNAME} processing statusjson.cgi" >> ${LOG_FILE}
-            STATE=`grep "last_hard_state.:" ${FILE} | cut -d ":" -f 2 | tr -cd [:digit:]` 2> /dev/null
-            OUTPUT=`grep "\"plugin_output.:" ${FILE} | tr -s ' ' | tr -d '"' |  sed -e 's/ plugin_output: //' -e 's/,$//'` 2> /dev/null
+            echo "$$ ${PROGNAME} processing statusjson.cgi" >> "${LOG_FILE}"
+            STATE=$(grep "last_hard_state.:" "${FILE}" | cut -d ":" -f 2 | tr -cd "[:digit:]") 2> /dev/null
+            OUTPUT=$(grep "\"plugin_output.:" "${FILE}" | tr -s ' ' | tr -d '"' |  sed -e 's/ plugin_output: //' -e 's/,$//') 2> /dev/null
         fi
     else
-        echo "$$ ${PROGNAME} statusjson.cgi empty, awk command against ${STATUS_FILE}" >> ${LOG_FILE}
+        echo "$$ ${PROGNAME} statusjson.cgi empty, awk command against ${STATUS_FILE}" >> "${LOG_FILE}"
     fi
 fi
 if [ "${STATE}X" == "X" ]; then
-    awk -v FS='\n' -v RS='\n\n' -v h_name="${HOST}" -v s_name="${SERVICE}" 'BEGIN {host="host_name="h_name; service="service_description="s_name; print host", "service;}{ if (match($0,host) && match($0,service)) { print "##" $0; } }' ${STATUS_FILE} > ${FILE}
-    STATE=`grep "last_hard_state=" ${FILE} | cut -d "=" -f 2 | tr -cd [:digit:]` 2> /dev/null
-    OUTPUT=`grep "plugin_output=" ${FILE} | cut -d "=" -f 2 | sed 's/,$//'` 2> /dev/null
+    awk -v FS='\n' -v RS='\n\n' -v h_name="${HOST}" -v s_name="${SERVICE}" 'BEGIN {host="host_name="h_name; service="service_description="s_name; print host", "service;}{ if (match($0,host) && match($0,service)) { print "##" $0; } }' ${STATUS_FILE} > "${FILE}"
+    STATE=$(grep "last_hard_state=" "${FILE}" | cut -d "=" -f 2 | tr -cd "[:digit:]") 2> /dev/null
+    OUTPUT=$(grep "plugin_output=" "${FILE}" | cut -d "=" -f 2 | sed 's/,$//') 2> /dev/null
 fi
-chmod 666 ${FILE}
+chmod 666 "${FILE}"
 #
-echo "$$ ${PROGNAME} state: ${STATE}, output: ${OUTPUT}" >> ${LOG_FILE}
+echo "$$ ${PROGNAME} state: ${STATE}, output: ${OUTPUT}" >> "${LOG_FILE}"
 # move output file to a single 'last' file
-cp -f ${FILE} /tmp/check_${SERVICE}_last
-rm ${FILE}
+cp -f "${FILE}" "/tmp/check_${SERVICE}_last"
+rm "${FILE}"
 #
 if [ "${STATE}X" != "X" ]; then
     if [ "${STATE}" == "${STATE_OK}" ]; then
@@ -141,13 +142,13 @@ if [ "${STATE}X" != "X" ]; then
         if [ "${OUTPUT}X" != "X" ]; then
             echo "${OUTPUT}"
         else
-            if [ ${ESCALATE} == "true" ]; then
+            if [ "X${ESCALATE}" == "Xtrue" ]; then
                 echo "CRITICAL"
             else
                 echo "WARNING"
             fi
         fi
-        if [ ${ESCALATE} == "true" ]; then
+        if [ "X${ESCALATE}" == "Xtrue" ]; then
             exit "${STATE_CRITICAL}"
         fi
         exit "${STATE_WARNING}"
